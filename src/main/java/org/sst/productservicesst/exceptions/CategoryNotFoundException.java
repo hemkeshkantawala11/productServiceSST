3a0e69b600cb2bc1a5a2d1a993b59c5efc422e68
hemkesh <hemkesh123kantawala@gmail.com>
2024-04-26 11:24:39 +0530

    Added repo

---
+++ src/main/java/org/sst/productservicesst/exceptions/CategoryNotFoundException.java
@@ -0,0 +1,7 @@
+package org.sst.productservicesst.exceptions;
+
+public class CategoryNotFoundException extends RuntimeException{
+    public CategoryNotFoundException(String message) {
+        super(message);
+    }
+}
